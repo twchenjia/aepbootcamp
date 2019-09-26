5d173f992947ddbec5d31be2cb80c107c4eaae0f
chenjia <jia.chen1@thoughtworks.com>
2019-09-26 14:05:20 +0800

    has finshed top 3 tasks

--- src/main/java/com/thoughtworks/parking/Park.java
+++ src/main/java/com/thoughtworks/parking/Park.java
@@ -1,11 +1,25 @@
 package com.thoughtworks.parking;
 
 public class Park {
-    public Park(String s) {
+
+    public Park() {
 
     }
 
-    public boolean parkCar(String ticket) {
-        return true;
+    public boolean parkCar(int car, int position) throws ParkException {
+        if (car < position) {
+            return true;
+        } else if (car == position) {
+            return false;
+        } else {
+            throw new ParkException();
+        }
+    }
+
+    static class ParkException extends Exception {
+
+        public ParkException() {
+            super();
+        }
     }
 }
--- src/test/java/com/thoughtworks/parking/ParkTest.java
+++ src/test/java/com/thoughtworks/parking/ParkTest.java
@@ -1,15 +1,32 @@
 package com.thoughtworks.parking;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParkTest {
     @Test
-    void should_park_in__when_parking_if_park_lot_has_position() {
-        Park park = new Park("");
-        boolean isPermit = park.parkCar("111");
+    void should_park_in_when_parking_if_parking_car_count_is_less_than_parking_position() throws Park.ParkException {
+        Park park = new Park();
+        boolean isPermit = park.parkCar(1, 2);
         assertThat(isPermit, is(true));
+    }
+
+    @Test
+    void should_not_park_in_when_parking_if_parking_car_count_is_equal_parking_position() throws Park.ParkException {
+        Park park = new Park();
+        boolean isPermit = park.parkCar(10, 10);
+        assertThat(isPermit, is(false));
+    }
+
+    @Test
+    void should_throw_pack_exception_when_parking_if_parking_car_count_is_more_than_parking_position() throws Park.ParkException {
+        Park park = new Park();
+        assertThrows(Park.ParkException.class, () -> park.parkCar(2, 1));
     }
 }
